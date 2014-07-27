5714f6aa9f2c870de82dcac3bfd2965235c318e1
Sebastian Weiss <dl3yc@darc.de>
2014-07-28 00:45:24 +0200

    add midiplayer process

--- midi/midiplayer.c
+++ midi/midiplayer.c
@@ -3,6 +3,7 @@
 #include <smf.h>
 #include <jack/jack.h>
 #include <jack/ringbuffer.h>
+#include <jack/midiport.h>
 
 struct midiplayer {
 	/* user data */
@@ -23,8 +24,10 @@ struct midiplayer {
 	int load_midi;
 	int midi_loaded;
 	char *midifile;
+	int rb_reset;
 	pthread_mutex_t sync_mutex;
 	pthread_mutex_t load_mutex;
+	pthread_mutex_t reset_mutex;
 };
 
 static struct midiplayer midiplayer_new = {
@@ -42,11 +45,72 @@ static struct midiplayer midiplayer_new = {
 	.load_midi = 0,
 	.midi_loaded = 0,
 	.midifile = NULL,
+	.rb_reset = 0,
 };
 
 static int midiplayer_process(jack_nframes_t nframes, void *arg)
 {
 	struct midiplayer *player = (struct midiplayer *) arg;
+	void* output_buffer = jack_port_get_buffer(player->port, nframes);
+	int read_space;
+	int i;
+	static int repeat_write = 0;
+	static uint8_t cmd[5];
+	char msg[16];
+	int32_t offset;
+	jack_transport_state_t state;
+	jack_position_t pos;
+
+	jack_midi_clear_buffer(output_buffer);
+	read_space = jack_ringbuffer_read_space(player->rb);
+
+	state = jack_transport_query(player->client, &pos);
+
+	if (state != JackTransportRolling)
+		return 0;
+
+	if (read_space == 0)
+		return 0;
+
+	if (player->rb_reset == 1) {
+		if (pthread_mutex_trylock(&player->reset_mutex)) {
+			repeat_write = 0;
+			player->rb_reset = 0;
+			pthread_mutex_unlock(&player->reset_mutex);
+		} else {
+			return 0;
+		}
+	}
+
+	i = 0;
+	while(i < read_space) {
+		if (!repeat_write)
+			jack_ringbuffer_read(player->rb, (char *) cmd, 5);
+		offset = (uint32_t) cmd[0] + ((uint32_t) cmd[1] << 8) + ((uint32_t) cmd[2] << 16) + ((uint32_t) cmd[3] << 24) - pos.frame;
+		if (offset < 0)
+			offset = 0;
+		if (offset > nframes) {
+			repeat_write = 1;
+			break;
+		}
+		repeat_write = 0;
+
+		jack_ringbuffer_read(player->rb, msg, cmd[4]);
+		jack_midi_event_write(output_buffer, offset, (jack_midi_data_t *) msg, cmd[4]);
+		printf("raw=%d\n", (uint32_t) cmd[0] + ((uint32_t) cmd[1] << 8) + ((uint32_t) cmd[2] << 16) + ((uint32_t) cmd[3] << 24));
+		printf("process: offset=%d pos.frame=%d\n", offset, pos.frame);
+		printf("cmd[0]=%2.0x cmd[1]=%2.0x cmd[2]=%2.0x cmd[3]=%2.0x\n", cmd[0], cmd[1], cmd[2], cmd[3]);
+
+#if 0
+		if (jack_midi_get_lost_event_count(player->port)) {
+			printf("process: lost event\n");
+		}
+#endif
+		i += 5 + cmd[4];
+	}
+
+	//jack_ringbuffer_reset(player->rb);
+
 	return 0;
 }
 
@@ -74,8 +138,13 @@ static int midiplayer_sync(jack_transport_state_t state, jack_position_t *pos, v
 static void *midiplayer_source(void *arg)
 {
 	struct midiplayer *player = (struct midiplayer *) arg;
+	smf_event_t *event;
 	int samplerate;
 	int ret;
+	uint8_t msg[5];
+	uint32_t offset;
+	int repeat_write = 0;
+	int rb_reset = 0;
 
 	while(!player->init_done);
 
@@ -102,6 +171,8 @@ static void *midiplayer_source(void *arg)
 		if ((player->handle_reposition) && !(player->reposition_handled)) {
 			if (pthread_mutex_trylock(&player->sync_mutex) == 0) {
 				ret = smf_seek_to_seconds(player->smf, (double) player->frame / samplerate);
+				rb_reset = 1;
+				printf("source: time:%f\n", (double) player->frame / samplerate);
 				if (ret == 0)
 					player->reposition_handled = 1;
 				else
@@ -110,10 +181,54 @@ static void *midiplayer_source(void *arg)
 			}
 		}
 
-		/* ... */
+		if (rb_reset) {
+			if (pthread_mutex_trylock(&player->reset_mutex) == 0) {
+				player->rb_reset = 1;
+				jack_ringbuffer_reset(player->rb);
+				rb_reset = 0;
+				pthread_mutex_unlock(&player->reset_mutex);
+			}
+		}
 
+		if (!repeat_write) {
+			event = smf_get_next_event(player->smf);
+			if (event == NULL)
+				continue;
+			if (smf_event_is_metadata(event))
+				continue;
+		}
+
+		printf("source: writing buffer...\n");
+
+		if (jack_ringbuffer_write_space(player->rb) < 5 + event->midi_buffer_length) {
+			printf("source: not enough space in ringbuf\n");
+			repeat_write = 1;
+			continue;
+		}
+		repeat_write = 0;
+
+		offset = (uint32_t) (event->time_seconds * samplerate);
+		if (offset < 0)
+			offset = 0;
+
+		printf("source: offset=%d\n", offset);
+
+		msg[0] = (uint8_t) (offset);
+		msg[1] = (uint8_t) (offset >> 8);
+		msg[2] = (uint8_t) (offset >> 16);
+		msg[3] = (uint8_t) (offset >> 24);
+		msg[4] = (uint8_t) event->midi_buffer_length;
+		printf("msg[0]=%2.0x msg[1]=%2.0x msg[2]=%2.0x msg[3]=%2.0x\n", msg[0], msg[1], msg[2], msg[3]);
+		ret = jack_ringbuffer_write(player->rb, (char *) msg, 5);
+		if (ret < 5) {
+			fprintf(stderr, "midiplayer: lost %d bytes\n", 5 - ret);
+		}
+
+		ret = jack_ringbuffer_write(player->rb, (const char *) event->midi_buffer, event->midi_buffer_length);
+		if (ret < event->midi_buffer_length) {
+			fprintf(stderr, "midiplayer: lost %d bytes\n", event->midi_buffer_length - ret);
+		}
 	}
-
 	return 0;
 }
 
@@ -126,6 +241,12 @@ int midiplayer_load(struct midiplayer *player, char *midifile)
 	return 0;
 }
 
+void midiplayer_free(struct midiplayer *player)
+{
+	jack_ringbuffer_free(player->rb);
+	smf_delete(player->smf);
+}
+
 int midiplayer_init(struct midiplayer *player)
 {
 	jack_status_t status;
@@ -135,22 +256,39 @@ int midiplayer_init(struct midiplayer *player)
 	ret = pthread_mutex_init(&player->sync_mutex, NULL);
 	if (ret != 0) {
 		fprintf(stderr, "midiplayer: can't init sync mutex\n");
+		return -1;
 	}
 
 	ret = pthread_mutex_init(&player->load_mutex, NULL);
 	if (ret != 0) {
 		fprintf(stderr, "midiplayer: can't init load mutex\n");
+		return -1;
+	}
+
+	ret = pthread_mutex_init(&player->reset_mutex, NULL);
+	if (ret != 0) {
+		fprintf(stderr, "midiplayer: can't init reset mutex\n");
+		return -1;
 	}
 
 	ret = pthread_create(&player->thread_id, NULL, midiplayer_source, player);
 	if (ret != 0) {
 		fprintf(stderr, "midiplayer: can't create source thread\n");
+		return -1;
 	}
 
 	pthread_detach(player->thread_id);
 	if (ret != 0) {
 		fprintf(stderr, "midiplayer: can't detach source thread\n");
+		return -1;
 	}
+
+	player->rb = jack_ringbuffer_create(1024);
+	if (player->rb == NULL) {
+		fprintf(stderr, "midiplayer: can't create jack ringbuffer\n");
+		return -1;
+	}
+
 	player->client = jack_client_open(player->name, options, &status, player->servername);
 	if (player->client == NULL) {
 		fprintf(stderr, "midiplayer: can't open jack client\n");
@@ -216,6 +354,8 @@ int main(int argc, char *argv[])
 
 	midiplayer_init(&player);
 	midiplayer_load(&player, argv[1]);
+
+	printf("midi loaded\n");
 
 	while(1);
 
